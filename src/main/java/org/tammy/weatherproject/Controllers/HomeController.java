2d97219dd697e36e12ee1e4e998dc939ea7344f7
ctam91 <ctam91@gmail.com>
2017-11-13 19:13:12 -0800

    initial commit

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -6,11 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.tammy.weatherproject.Models.WeatherData;
-import org.tammy.weatherproject.Models.Weather;
-import org.tammy.weatherproject.Models.WeatherForecast;
-
-import java.util.ArrayList;
 
 
 @Controller
@@ -20,36 +15,12 @@ public class HomeController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model){
-        String city = "Seattle";
-        String state = "WA";
 
-        String image = "http://api.wunderground.com/api/cb5d7b2fbd91dacc/animatedradar/q/"+ state + "/" + city + ".gif?newmaps=1";
-
-        Weather theWeather = WeatherData.fetchWeatherData("http://api.wunderground.com/api/cb5d7b2fbd91dacc/conditions/q/"+state +"/"+ city +".json");
-
-        ArrayList<WeatherForecast> forecasts = WeatherData.fetchWeatherForecast("http://api.wunderground.com/api/cb5d7b2fbd91dacc/forecast/q/"+ state +"/" + city + ".json");
-
-        model.addAttribute("forecasts", forecasts);
-        model.addAttribute("imageURL", image);
-        model.addAttribute("weather",theWeather);
         return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String index(Model model, @RequestParam String search){
-        String state = WeatherData.findState(search);
-        String city = WeatherData.findCity(search);
-
-        String image = "http://api.wunderground.com/api/cb5d7b2fbd91dacc/animatedradar/q/"+ state + "/" + city + ".gif?newmaps=1";
-
-        Weather theWeather = WeatherData.fetchWeatherData("http://api.wunderground.com/api/cb5d7b2fbd91dacc/conditions/q/"+state +"/"+ city +".json");
-
-        ArrayList<WeatherForecast> forecasts = WeatherData.fetchWeatherForecast("http://api.wunderground.com/api/cb5d7b2fbd91dacc/forecast/q/"+ state +"/" + city + ".json");
-
-        model.addAttribute("forecasts", forecasts);
-        model.addAttribute("imageURL", image);
-        model.addAttribute("weather",theWeather);
-
         return "index";
     }
 
--- src/main/java/org/tammy/weatherproject/Models/Weather.java
+++ src/main/java/org/tammy/weatherproject/Models/Weather.java
@@ -6,16 +6,10 @@ public class Weather {
 
     private String temperature;
     private String location;
-    private String observationTime;
-    private String weatherDescription;
-    private String iconURL;
 
-    public Weather(String temperature, String location, String observationTime, String description, String iconURL) {
+    public Weather(String temperature, String location) {
         this.temperature = temperature;
         this.location = location;
-        this.observationTime = observationTime;
-        this.weatherDescription = description;
-        this.iconURL = iconURL;
     }
 
 
@@ -35,27 +29,4 @@ public class Weather {
         this.location = mLocation;
     }
 
-    public String getObservationTime() {
-        return observationTime;
-    }
-
-    public void setObservationTime(String observationTime) {
-        this.observationTime = observationTime;
-    }
-
-    public String getDescription() {
-        return weatherDescription;
-    }
-
-    public void setDescription(String description) {
-        this.weatherDescription = description;
-    }
-
-    public String getIconURL() {
-        return iconURL;
-    }
-
-    public void setIconURL(String iconURL) {
-        this.iconURL = iconURL;
-    }
 }
