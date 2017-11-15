40109b124c6d40a309cd2206713c5c5d1bf46371
ctam91 <ctam91@gmail.com>
2017-11-15 13:32:58 -0800

    update index post

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -3,9 +3,14 @@ package org.tammy.weatherproject.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.tammy.weatherproject.Models.Direction;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -15,13 +20,13 @@ public class HomeController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model){
-        String url = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyBGr4S6D6vPzukv48fFiicLaKcEurYkCIg&origin=Oslo+Norway&destination=Telemar+Norway&avoid=tolls|highways";
-        model.addAttribute("url", url);
+        model.addAttribute("direction", new Direction());
         return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String index(Model model, @RequestParam String search){
+    public String index(@ModelAttribute @Valid Direction direction, Errors errors, Model model){
+        model.addAttribute("direction", direction);
         return "index";
     }
 
