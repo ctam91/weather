5bd353262e7ef2dd75c1e6c9e10620fd2b552ab1
ctam91 <ctam91@gmail.com>
2017-11-16 11:33:26 -0800

    add Google Directions

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.tammy.weatherproject.Models.Direction;
 
 import javax.validation.Valid;
@@ -26,7 +25,7 @@ public class HomeController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String index(@ModelAttribute @Valid Direction direction, Errors errors, Model model){
-        model.addAttribute("direction", direction);
+
         return "index";
     }
 
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,37 +1,97 @@
-  function initMap() {
-    var chicago = {lat: 41.85, lng: -87.65};
-    var indianapolis = {lat: 39.79, lng: -86.14};
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          mapTypeControl: false,
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: chicago,
-      zoom: 7
-    });
+    //var result = $('form').serialize();
+   // var array = result.split("&");
+    //var origin_array = array[0].split("=");
+    //var origin = origin_array[1];
 
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-      map: map
-    });
+    //var destination_array= array[1].split("=");
+    //var destination = destination_array[1];
 
-    // Set destination, origin and travel mode.
-    var request = {
-      destination: indianapolis,
-      origin: chicago,
-      travelMode: 'DRIVING'
-    };
 
-    // Pass the directions request to the directions service.
-    var directionsService = new google.maps.DirectionsService();
-    directionsService.route(request, function(response, status) {
-      if (status == 'OK') {
-        // Display the route on the map.
-        directionsDisplay.setDirections(response);
+        new AutocompleteDirectionsHandler(map);
       }
-    });
 
-    var result = $('form').serialize();
-    console.log(result)
-    var array = result.split("&");
-    var origin_array = array[0].split("=");
-    var origin = origin_array[1];
-    var destination_array= array[1].split("=");
-    var destination = destination_array[1];
-  }
+       /**
+        * @constructor
+       */
+      function AutocompleteDirectionsHandler(map) {
+        this.map = map;
+        this.originPlaceId = null;
+        this.destinationPlaceId = null;
+        this.travelMode = 'WALKING';
+        var originInput = document.getElementById('origin-input');
+        var destinationInput = document.getElementById('destination-input');
+        var modeSelector = document.getElementById('mode-selector');
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        this.directionsDisplay.setMap(map);
+
+        var originAutocomplete = new google.maps.places.Autocomplete(originInput, {placeIdOnly: true});
+        var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput, {placeIdOnly: true});
+
+        this.setupClickListener('changemode-walking', 'WALKING');
+        this.setupClickListener('changemode-transit', 'TRANSIT');
+        this.setupClickListener('changemode-driving', 'DRIVING');
+
+        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+      }
+
+      // Sets a listener on a radio button to change the filter type on Places
+      // Autocomplete.
+      AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+        var radioButton = document.getElementById(id);
+        var me = this;
+        radioButton.addEventListener('click', function() {
+          me.travelMode = mode;
+          me.route();
+        });
+      };
+
+      AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+        var me = this;
+        autocomplete.bindTo('bounds', this.map);
+        autocomplete.addListener('place_changed', function() {
+          var place = autocomplete.getPlace();
+          if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+          }
+          if (mode === 'ORIG') {
+            me.originPlaceId = place.place_id;
+          } else {
+            me.destinationPlaceId = place.place_id;
+          }
+          me.route();
+        });
+
+      };
+
+      AutocompleteDirectionsHandler.prototype.route = function() {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+          return;
+        }
+        var me = this;
+
+        this.directionsService.route({
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        }, function(response, status) {
+          if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      };
