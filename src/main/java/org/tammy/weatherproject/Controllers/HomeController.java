5c20f95eaf644ece4c2539c065d1b56db3192462
ctam91 <ctam91@gmail.com>
2017-11-17 17:53:13 -0800

    update controller

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -4,12 +4,8 @@ package org.tammy.weatherproject.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.tammy.weatherproject.Models.Direction;
-
-import javax.validation.Valid;
 
 
 @Controller
@@ -19,13 +15,11 @@ public class HomeController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model){
-        model.addAttribute("direction", new Direction());
         return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String index(@ModelAttribute @Valid Direction direction, Errors errors, Model model){
-
+    public String index(Errors errors, Model model){
         return "index";
     }
 
