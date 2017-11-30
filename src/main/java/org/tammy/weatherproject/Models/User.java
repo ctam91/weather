e086a63df6d18af5dbc0c56888cb1c4e3244ee7b
ctam91 <ctam91@gmail.com>
2017-11-30 15:26:09 -0800

    add User class

--- src/main/java/org/tammy/weatherproject/Models/User.java
+++ src/main/java/org/tammy/weatherproject/Models/User.java
@@ -1,4 +1,52 @@
 package org.tammy.weatherproject.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15,  message = "Please enter a username with at least 3 characters")
+    private String name;
+
+    @NotNull
+    @Size(min=8,  message = "Please enter a password with at least 8 characters")
+    private String password;
+
+    public User() {}
+
+    public User(String name) {
+        this();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
+
