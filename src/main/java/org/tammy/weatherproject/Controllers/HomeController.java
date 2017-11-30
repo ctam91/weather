530307148eb5db8a87aec4943c55bd5f0543ffd0
ctam91 <ctam91@gmail.com>
2017-11-30 15:25:36 -0800

    update todos

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //TODO: add db for user
 //TODO: add login/signup pages
 //TODO: store user preferences
+//TODO: add custom weather icons
 
 @Controller
 @RequestMapping("")
