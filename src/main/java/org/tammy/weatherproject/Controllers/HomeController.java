86a700787b9e48985eceb680fb89423cb32ce0eb
ctam91 <ctam91@gmail.com>
2017-11-30 16:03:05 -0800

    update pathways

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -1,11 +1,19 @@
 package org.tammy.weatherproject.Controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.tammy.weatherproject.Models.User;
+import org.tammy.weatherproject.Models.UserData;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 
 //TODO: fix navbar transparency
@@ -21,14 +29,93 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 public class HomeController {
 
+    @Autowired
+    private UserData userData;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model){
+    public String index(Model model) {
         return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String index(Errors errors, Model model){
+    public String index(Errors errors, Model model) {
         return "index";
     }
+
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
+    public String addUser(Model model) {
+        model.addAttribute("title", "Sign up!");
+        model.addAttribute("user", new User());
+
+        return "signup";
+    }
+
+    @RequestMapping(value = "signUp", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute @Valid User user, Errors errors, Model model, RedirectAttributes ra, HttpSession session) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Sign up!");
+            return "userSignUp";
+        }
+
+        userData.generateUserList();
+
+        if (userData.checkUserExists(user)) {
+            model.addAttribute("title", "Sign up!");
+            model.addAttribute("userError", "User name exists. Please select another.");
+            return "userSignUp";
+        }
+
+        userData.saveNewUser(user);
+        session.setAttribute("loggedInUser", user);
+        ra.addFlashAttribute("username", "Welcome " + user.getName());
+        return "redirect:";
+    }
+
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String logIn(Model model){
+
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "Welcome to MatchFour!" );
+        return "userLogin";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String logIn(@ModelAttribute @Valid User user, Errors errors, Model model, RedirectAttributes ra, HttpSession session) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Welcome to MatchFour!" );
+            model.addAttribute("user", new User());
+            return "userLogin";
+        }
+
+        userData.generateUserList();
+
+        if (!userData.checkUserExists(user)) {
+
+            model.addAttribute("title", "Welcome to MatchFour!" );
+            model.addAttribute("userError", "User name does not exist. Please sign up!");
+            model.addAttribute("user", new User());
+            return "login";
+        }
+
+        else if (userData.checkUserExists(user) && !userData.verifyUserPassword(user)) {
+
+            model.addAttribute("title", "Welcome to MatchFour!" );
+            model.addAttribute("passwordError", "Invalid password!");
+            model.addAttribute("user", new User());
+            return "login";
+
+        }
+
+        else {
+
+            session.setAttribute("loggedInUser", user);
+            ra.addFlashAttribute("username", "Welcome " + user.getName());
+            return "redirect:";
+        }
+    }
+
 
 }
